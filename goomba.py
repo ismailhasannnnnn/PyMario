06bbbb9cf6c7c762089c4ab1611f865034532506
ismailhasan <ismailhasan0132@gmail.com>
2022-04-05 19:43:41 -0700

    goomba now moves

--- goomba.py
+++ goomba.py
@@ -27,17 +27,26 @@ class Goomba(Sprite):
         self.timer = Timer(self.goomba_images, 0, 500, True)
         self.screen_rect = self.screen.get_rect()
 
-        self.v = Vector()
+        self.v = Vector(-4, 0)
 
     def update(self):
         self.mario = self.game.mario
         self.enemies = self.game.enemies
-        self.rect.x = self.ul[0] + self.enemies.bg_x
+        self.center = Vector(self.rect.centerx, self.rect.centery)
+        self.lastBg_x = 0
+        self.center += self.v
+
+        self.rect.centerx += self.enemies.bg_x - self.lastBg_x
+        self.lastBg_x = self.game.bg_x
+
         collisions = pg.sprite.spritecollide(self.mario, self.enemies.enemies, True)
-        # print(f'Goomba: {self.rect.x}         {self.rect.y}')
-        # print(collisions)
+
+        self.rect.centerx, self.rect.centery = self.center.x, self.center.y
+
 
     def draw(self):
         self.image = self.timer.image()
         self.enemies = self.game.enemies
-        self.screen.blit(self.image, (self.ul[0] + self.enemies.bg_x, self.ul[1]))
+        self.screen.blit(self.image, (self.rect.x + self.enemies.bg_x, self.rect.y))
+
+
